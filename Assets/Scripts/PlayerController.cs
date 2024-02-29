ac38dd1e91f91479f4131a495011d2af86816348
Simon <hatchery@hotmail.de>
2024-02-29 09:25:05 +0100

    PlayerController setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,166 @@
+using System.Collections;
+using UnityEngine;
+
+//TODO: if you still aren't making use of partial controllers make it not partial
+public partial class PlayerController : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] float playerRadius;
+
+    public enum PlayerState { CanMove, CannotMove };
+    public enum PlayerView { Freelook, Constrained };
+    [Header("PlayerState")]
+    [SerializeField] private PlayerState ePlayerState;
+    [SerializeField] private PlayerView ePlayerView;
+    [SerializeField] private float playerViewConstraints;
+
+    [Header("Physics")]
+    [SerializeField] bool isGrounded;
+    [SerializeField] Vector3 gravity;   // this needs to be vector3 to avoid a runtime cast
+    [SerializeField] float gravityVelocityMax = 5f;
+    [SerializeField] float gravityAcceleration = 0.1f;
+
+    [Header("Controls")]
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode interactionKey = KeyCode.F;
+    public KeyCode abortKey = KeyCode.Space;
+    public KeyCode pauseKey = KeyCode.Escape;
+    public KeyCode wInput = KeyCode.W;
+    public KeyCode sInput = KeyCode.S;
+    public KeyCode dInput = KeyCode.A;
+    public KeyCode aInput = KeyCode.D;
+    public float mouseSensitivity = 1f;
+    [SerializeField] private float playerAcceleration = 1f;
+
+    [Header("Player Stats")]
+    [SerializeField] private Vector2 playerInput;
+    private readonly float _lowerLookLimit = 75.0f;
+    private readonly float _upperLookLimit = 75.0f;
+    private float _xRotation;
+    private float _yRotation;
+    private bool _overwriteInAction;
+
+    Transform _transformCache;   // does this actually save performance? rider says so, and surely rider isn't lying
+
+    private void Awake()
+    {
+        _transformCache = transform;
+    }
+
+    void Start()
+    {
+        controller = this.GetComponent<CharacterController>();
+        playerCamera = this.GetComponentInChildren<Camera>();
+        playerRadius = controller.radius;
+        ePlayerState = PlayerState.CanMove;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        //PauseGame(); not yet implemented
+
+        IsGroundedCheck();
+
+        MouseLook();
+        WasdGetSet();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_overwriteInAction)
+        {
+            //Update
+            PlayerGravityIncrement();   //TODO: this is still fucky as hell
+        }
+    }
+
+    private void MouseLook()
+    {
+        _xRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        _xRotation = Mathf.Clamp(_xRotation, -_upperLookLimit, _lowerLookLimit);
+        _yRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
+
+        playerCamera.transform.localRotation = Quaternion.Euler(_xRotation, 0, 0);
+
+        transform.rotation *= Quaternion.Euler(0, _yRotation, 0);
+
+        if (ePlayerView == PlayerView.Constrained)
+            if (transform.rotation.eulerAngles.y > playerViewConstraints + 60)
+                transform.rotation = Quaternion.Euler(0, playerViewConstraints + 60, 0);
+            else if (transform.rotation.eulerAngles.y < playerViewConstraints - 60)
+                transform.rotation = Quaternion.Euler(0, playerViewConstraints - 60, 0);
+    }
+
+    private void WasdGetSet()
+    {
+
+        if (ePlayerState != PlayerState.CanMove) return;
+
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+        controller.Move(playerAcceleration * Time.deltaTime * (playerInput.x * _transformCache.right + playerInput.y * _transformCache.forward + gravity.y * _transformCache.up));
+    }
+
+    private void IsGroundedCheck()
+    {
+        //TODO: why in god's name does -transform.up not actually go down and has to be hacked like this
+        isGrounded = Physics.BoxCast(controller.transform.position,
+                                        new Vector3(playerRadius * 0.5f, 0.05f, playerRadius * 0.5f),
+                                        -_transformCache.up * 1000,
+                                        _transformCache.rotation,
+                                        (controller.transform.localScale.y * (controller.height / 2)) + 0.2f);
+    }
+
+    private void PlayerGravityIncrement()
+    {
+        if (isGrounded)
+            gravity.y = -0.005f;
+        else
+            {
+                gravity.y -= gravityAcceleration * Time.deltaTime;
+                gravity.y = Mathf.Clamp(gravity.y, -gravityVelocityMax, 0);
+            }
+    }
+
+    #region Getters/Setters
+    public int SetPlayerState(PlayerState newState)
+    {
+        if (ePlayerState == newState)
+            return 0;
+
+        ePlayerState = newState;
+        return 1;
+    }
+    
+    /// for "teleporting" player - will circumvent movement logic for a few frames
+    public void SetNewPlayerPosition(Vector3 newPlayerPosition)
+    {
+        IEnumerator coroutine = MovePlayerWithOverwrite(2, newPlayerPosition); // yes, this needs to be called with 2 frames or else we get race conditions. no, i don't know why
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator MovePlayerWithOverwrite(int amountOfFrames, Vector3 newPlayerPosition)
+    {
+        _overwriteInAction = true;
+        transform.position = newPlayerPosition;
+
+        for (int i = 0;  i < amountOfFrames; i++)
+            yield return null;
+
+        transform.position = newPlayerPosition;
+        _overwriteInAction = false;
+    }
+
+    // reminder that yRotation needs to be given as an absolute value, no matter what the transform shows
+    public void SetPlayerView(PlayerView newView, float yRotation)
+    {
+        playerViewConstraints = yRotation;
+        ePlayerView = newView;
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+    #endregion
+}
