f763b1070f7a12b9783da39a780180964804c4c6
Simon <hatchery@hotmail.de>
2024-03-07 23:53:21 +0100

    Riverside arena flickeroo implemented

---
+++ Assets/Scripts/ArenaTriggers/ArenaStarter_Riverside.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using AI;
+using PlayerScript;
+using UnityEngine;
+
+namespace ArenaTriggers
+{
+    public class ArenaStarterRiverside : MonoBehaviour
+    {
+        [SerializeField] private List<PrototypeAI> enemiesToActivate;
+        [SerializeField] private Animator entranceAnimator;
+        [SerializeField] private Animator exitAnimator;
+        [SerializeField] private GameObject oldGeometry;
+        [SerializeField] private Material waterMaterial;
+        [SerializeField] private Color startColor = new Color(0.2235293f, 0.5372549f, 0.7254902f, 0.6470588f);
+        [SerializeField] private Color targetColor = new Color(0.4823529f, 0.05490194f, 0.01176471f, 0.6470588f);
+        [SerializeField] private float startMetallic = 0.3f;
+        [SerializeField] private float targetMetallic = 0.95f;
+        [SerializeField] private List<Light> riverLights;
+        private bool _hasEntered;
+        private static readonly int IsTriggered = Animator.StringToHash("IsTriggered");
+        private bool _fightIsOver;
+        private static readonly int WaterColor = Shader.PropertyToID("_Water_Color");
+        private static readonly int Metallic = Shader.PropertyToID("_Metallic");
+        private static readonly int Transparency = Shader.PropertyToID("_Transparency");
+
+        private void Start()
+        {
+            waterMaterial.SetColor(WaterColor, new Color(0.2235293f, 0.5372549f, 0.7254902f, 0.6470588f));
+        }
+
+        private void FixedUpdate()
+        {
+            CheckForSurvivors();
+        }
+
+        private void CheckForSurvivors()
+        {
+            if (!_fightIsOver)
+            {
+                foreach (var enemy in enemiesToActivate)
+                {
+                    if (enemy.Alive) return;
+                }
+            }
+
+            _fightIsOver = true;
+            ReleaseArena();
+        }
+
+        private void ReleaseArena()
+        {
+            foreach (var lighty in riverLights)
+            {
+                lighty.color = Color.white;
+            }
+            
+            waterMaterial.SetColor(WaterColor, startColor);
+            waterMaterial.SetFloat(Metallic, 0.3f);
+            waterMaterial.SetFloat(Transparency, 0.995f);
+            
+            exitAnimator.SetBool(IsTriggered, true);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (_hasEntered) return;
+            if (other.gameObject.GetComponent<PlayerController>() == null)
+                return;
+        
+            entranceAnimator.SetBool(IsTriggered, true);
+            Invoke(nameof(DeactivateOldGeometry), 1f);
+            ThrowTheTea();
+
+            _hasEntered = true;
+        }
+
+        private void StartArena()
+        {
+            foreach (var enemy in enemiesToActivate)
+            {
+                enemy.SetAlert(true);
+            }
+        }
+        
+        private void ThrowTheTea()
+        {
+            StartCoroutine(ThrowTheTeaRoutine());
+        }
+
+        private IEnumerator ThrowTheTeaRoutine()
+        {
+            for (int i = 1; i < 21; i++)
+            {
+                yield return new WaitForSeconds(1f / i);
+                waterMaterial.SetColor(WaterColor, startColor);
+                waterMaterial.SetFloat(Metallic, startMetallic);
+                waterMaterial.SetFloat(Transparency, 0.995f);
+                foreach (var lighty in riverLights)
+                {
+                    lighty.color = Color.white;
+                }
+                
+                yield return new WaitForSeconds(1f / i);
+                waterMaterial.SetColor(WaterColor, targetColor);
+                waterMaterial.SetFloat(Metallic, targetMetallic);
+                waterMaterial.SetFloat(Transparency, 1f);
+                foreach (var light in riverLights)
+                {
+                    light.color = targetColor;
+                }
+                
+                startMetallic = Mathf.Lerp(startMetallic, targetMetallic, 0.1f);
+            }
+
+            StartArena();
+        }
+
+        private void DeactivateOldGeometry()
+        {
+            oldGeometry.SetActive(false);
+        }
+    }
+}
